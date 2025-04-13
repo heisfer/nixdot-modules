0457aa32bc1fcce33b05b3fa0f13718439d02ae4
heisfer <heisfer@refract.dev>
2025-04-13 21:59:58 +0300

    nixos: swayosd extended

--- nixos/services/swayosd.nix
+++ nixos/services/swayosd.nix
@@ -1,21 +1,48 @@
-# CREDIT:
-# https://github.com/NixOS/nixpkgs/issues/280041#issuecomment-2474583192
-
 {
-  config,
   lib,
+  config,
   pkgs,
+  inputs,
   ...
 }:
 let
-  inherit (lib) mkEnableOption mkIf mkPackageOption;
+  inherit (lib.options) mkEnableOption mkPackageOption mkOption;
+  inherit (lib.modules) mkIf;
+  inherit (lib.meta) getExe';
+  inherit (lib.types)
+    nullOr
+    either
+    lines
+    path
+    ;
   cfg = config.services.swayosd;
+
+  tomlFormat = pkgs.formats.toml { };
 in
 {
 
   options.services.swayosd = {
-    enable = mkEnableOption "swayosd services";
+    enable = mkEnableOption "SwayOSD";
     package = mkPackageOption pkgs "swayosd" { };
+
+    backend = mkOption {
+      type = tomlFormat.type;
+      default = { };
+      description = "SwayOSD backend settings";
+    };
+
+    config = mkOption {
+      type = tomlFormat.type;
+      default = { };
+      description = "SwayOSD backend settings";
+    };
+
+    style = mkOption {
+      type = nullOr (either path lines);
+      default = null;
+      description = "SwayOSD style";
+    };
+
   };
 
   config = mkIf cfg.enable {
@@ -23,6 +50,7 @@ in
     environment.systemPackages = [ cfg.package ];
     services.udev.packages = [ cfg.package ];
 
+    #backend
     systemd.services.swayosd-libinput-backend = {
       description = "SwayOSD LibInput backend for listening to certain keys like CapsLock, ScrollLock, VolumeUp, etc.";
       documentation = [ "https://github.com/ErikReider/SwayOSD" ];
@@ -33,10 +61,37 @@ in
       serviceConfig = {
         Type = "dbus";
         BusName = "org.erikreider.swayosd";
-        ExecStart = "${cfg.package}/bin/swayosd-libinput-backend";
+        ExecStart = getExe' cfg.package "swayosd-libinput-backend";
         Restart = "on-failure";
       };
     };
 
+    # User Service
+    systemd.user.services.swayosd = {
+      description = "Swayosd frontend server";
+      partOf = [ "graphical-session.target" ];
+      serviceConfig = {
+        ExecStart = getExe' cfg.package "swayosd-server";
+        Restart = "always";
+        RestartSec = "5s";
+      };
+      wantedBy = [ "graphical-session.target" ];
+    };
+
+    environment.etc = {
+      "xdg/swayosd/backend.toml" = mkIf (cfg.backend != { }) {
+        text = tomlFormat.generate "swayosd-backend" cfg.backend;
+      };
+      "xdg/swayosd/config.toml" = mkIf (cfg.config != { }) {
+        text = tomlFormat.generate "swayosd-config" cfg.config;
+      };
+      "xdg/swayosd/style.css".source =
+        if builtins.isPath cfg.style || lib.isStorePath cfg.style then
+          cfg.style
+        else
+          pkgs.writeText "swayosd-style.css" cfg.style;
+    };
+
   };
+
 }
